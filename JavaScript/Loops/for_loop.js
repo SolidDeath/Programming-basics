12786660590e513b46c5cbb4d6f7c667bc53c85e
Ernests <ernests.dane@gmail.com>
2023-05-18 00:16:49 +0300

    Loops

---
+++ JavaScript/Loops/for_loop.js
@@ -0,0 +1,60 @@
+/*
+    Lesson: For Loops
+
+    In JavaScript, a "for loop" is a control flow statement that allows code to be repeatedly executed. A for loop is characterized by three statements (usually) enclosed in parentheses and separated by semicolons:
+
+    1. The initialization statement: This is executed before the loop starts.
+    2. The condition statement: The loop will continue as long as this condition is true.
+    3. The final-expression statement: This is executed at the end of each loop iteration.
+
+    Here's the syntax:
+
+    for ([initialization]; [condition]; [final-expression]) {
+    // code block to be executed
+    }
+
+    The initialization statement is executed only once before the start of the loop. The condition statement is evaluated before each loop iteration. If it returns true, the loop continues; otherwise, the loop stops. After every iteration, the final-expression is executed.
+
+
+    Easy Level:
+        1. Write a for loop that prints numbers from 1 to 5.
+*/
+
+// Your code here
+
+/*
+    Medium Level:
+        2. Write a for loop that calculates the sum of all numbers from 1 to 10 and stores the result in a variable called total. Then, print the value of total after the loop finishes.
+*/
+
+// Your code here
+
+/*
+    Hard Level:
+        3. Write a for loop that iterates from 0 to 100. For each iteration, check if the current number is divisible by 3 and 5. If it is, print "FizzBuzz". If it's only divisible by 3, print "Fizz". If it's only divisible by 5, print "Buzz". If none of these conditions are met, print the number itself.
+*/
+
+// Your code here
+
+/*
+    Very Hard Level:
+        4. Write a for loop that generates the Fibonacci sequence up to the 10th term. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms. Print the sequence at the end of the loop.
+*/
+
+// Your code here
+
+/*
+    And here's the HTML page setup:
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript Lesson</title>
+    </head>
+    <body>
+        <script src="your-js-file.js"></script>
+    </body>
+</html>
+
+    Make sure to replace "your-js-file.js" with the actual name of your JavaScript file.
+*/
---
+++ JavaScript/Loops/while_loop.js
@@ -0,0 +1,59 @@
+/*
+    Lesson: While Loops
+
+    A "while loop" is a control flow statement that allows code to be executed repeatedly based on a given condition. The loop can be thought of as a repeating 'if' statement. The test condition is checked first, and if it's true, the code block within the loop is executed. The process continues until the test condition evaluates to false.
+
+    Here's the syntax:
+
+    while (condition) {
+    // code block to be executed
+    }
+
+    The condition is evaluated before each iteration. If it's true, the loop continues. If it's false, the loop ends, and the program control moves to the next statement after the loop.
+
+    Now, let's get into some tasks!
+
+    Easy Level:
+        1. Write a while loop that prints numbers from 1 to 5.
+*/
+
+// Your code here
+
+/*
+    Medium Level:
+    2. Write a while loop that calculates the sum of all numbers from 1 to 10 and stores the result in a variable called total. Then, print the value of total after the loop finishes.
+*/
+
+// Your code here
+
+/*
+    Hard Level:
+    3. Write a while loop that iterates from 0 to 100. For each iteration, check if the current number is divisible by 3 and 5. If it is, print "FizzBuzz". If it's only divisible by 3, print "Fizz". If it's only divisible by 5, print "Buzz". If none of these conditions are met, print the number itself.
+*/
+
+// Your code here
+
+/*
+    Very Hard Level:
+    4. Write a while loop that generates the Fibonacci sequence up to the 10th term. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms. Print the sequence at the end of the loop.
+*/
+
+// Your code here
+
+
+/*
+    Just like before, the HTML page setup would look like this:
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript Lesson</title>
+    </head>
+    <body>
+        <script src="your-js-file.js"></script>
+    </body>
+</html>
+
+Replace "your-js-file.js" with the actual name of your JavaScript file.
+
+*/
