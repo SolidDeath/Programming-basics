0a2ea8443250c3a9ec69eb71260bb0733c694846
Ernests <ernests.dane@gmail.com>
2023-05-21 16:18:31 +0300

    Changed advanced array exercises

--- JavaScript/Arrays/Advanced arrays/advanced_arr.js
+++ JavaScript/Arrays/Advanced arrays/advanced_arr.js
@@ -5,25 +5,26 @@
 
 /*
     ==================== Easy Level: ====================
-    Create an array of numbers and use the `map` function to square each number in the array.
+    Given an array of integers, use the `map` function to add 5 to each number in the array.
 */
 
 
 
 /*
     ==================== Medium Level: ====================
-    From an array of numbers, use the `filter` function to filter out odd numbers.
+    From an array of numbers, use the `filter` function to keep only numbers that are greater than or equal to 20.
 */
 
 
 
 /*
     ==================== Hard Level: ====================
-    Use the `reduce` method to find the product of an array of numbers.
+    Given an array of prices (as numbers), use the `reduce` method to find the total cost.
 */
+
 
 
 /*
     ==================== Very Hard Level: ====================
-    Given an array of people objects with properties `name` and `age`, write a function that checks if every person is above 18 years old using the `every` method. Also find the first person who is older than 30 using the `find` method.
+    You have an array of objects, where each object represents a person with a `name` and `numberOfBooksRead` property. Use the `filter` method to find all people who have read more than 10 books, and then use `map` to create an array of their names.
 */
